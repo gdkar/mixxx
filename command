875c6944c4b1108af660ae188f7465ce42cdf083
gabriel d. karpman <gdkar@mit.edu>
2015-05-15 23:26:23 -0400

    tweaks

---
+++ command
@@ -0,0 +1,1 @@
+CC=clang CXX=clang++ CCFLAGS=' -std=gnu14 ' CXXFLAGS=' -std=gnu++14 -Wno-inconsistent-missing-override  ' scons optimize=native mad=0 qt5=1 color=1 verbose=1 qdebug=1 ffmpeg=1 build=release mpg123=1   $@
--- src/control/controlvalue.h
+++ src/control/controlvalue.h
@@ -11,49 +11,6 @@
 #include "util/compatibility.h"
 #include "util/assert.h"
 
-// for look free access, this value has to be >= the number of value using threads
-// value must be a fraction of an integer
-const int cRingSize = 8;
-// there are basicly unlimited readers allowed at each ring element
-// but we have to count them so max() is just fine.
-// NOTE(rryan): Wrapping max with parentheses avoids conflict with the max macro
-// defined in windows.h.
-const int cReaderSlotCnt = (std::numeric_limits<int>::max)();
-
-// A single instance of a value of type T along with an atomic integer which
-// tracks the current number of readers or writers of the slot. The value
-// m_readerSlots starts at cReaderSlotCnt and counts down to 0. If the value is
-// 0 or less then reads to the value fail because there are either too many
-// readers or a write is occurring. A write to the value will fail if
-// m_readerSlots is not equal to cReaderSlotCnt (e.g. there is an active
-// reader).
-template<typename T>
-class ControlRingValue {
-  public:
-    ControlRingValue()
-        : m_value(T()),
-          m_readerSlots(cReaderSlotCnt) {}
-    bool tryGet(T* value) const {
-        // Read while consuming one readerSlot
-        bool hasSlot = (m_readerSlots.fetchAndAddAcquire(-1) > 0);
-        if (hasSlot) {*value = m_value;}
-        (void)m_readerSlots.fetchAndAddRelease(1);
-        return hasSlot;
-    }
-    bool trySet(const T& value) {
-        // try to lock this element entirely for reading
-        if (m_readerSlots.testAndSetAcquire(cReaderSlotCnt, 0)) {
-            m_value = value;
-            m_readerSlots.fetchAndAddRelease(cReaderSlotCnt);
-            return true;
-        }
-        return false;
-   }
-  private:
-    T m_value;
-    mutable QAtomicInt m_readerSlots;
-};
-
 // shared pointer based implementation for all Types sizeof(T) > sizeof(void*)
 
 // An implementation of ControlValueAtomicBase for non-atomic types T. Uses a
@@ -74,8 +31,7 @@ class ControlValueAtomicBase {
     inline T getValue() const {
         T value;
         QSharedPointer<T> data(m_data);
-        if(data)
-          value = *data;
+        if(data) value = *data;
         return value;
     }
     inline void setValue(const T& value) {
@@ -98,7 +54,6 @@ class ControlValueAtomicBase {
   private:
     QSharedPointer<T>   m_data;
 };
-
 // Specialized template for types that are deemed to be atomic on the target
 // architecture. Instead of using a read/write ring to guarantee atomicity,
 // direct assignment/read of an aligned member variable is used.
@@ -141,11 +96,7 @@ class ControlValueAtomic :  public ControlValueAtomicBase<T, sizeof(T) <= sizeof
     ControlValueAtomic(const T& t):ControlValueAtomicBase<T,sizeof(T)<=sizeof(quintptr)>(t){}
 };
 template<typename T>
-inline void swap(ControlValueAtomic<T> &lhs, T&rhs){
-  lhs.swap(rhs);
-}
+inline void swap(ControlValueAtomic<T> &lhs, T&rhs){lhs.swap(rhs);}
 template<typename T>
-inline void swap(T &lhs, ControlValueAtomic<T>&rhs){
-  lhs.swap(rhs);
-}
+inline void swap(T &lhs, ControlValueAtomic<T>&rhs){lhs.swap(rhs);}
 #endif /* CONTROLVALUE_H */
