391cf116183041e3d800dfbd07ff97621871808e
gabriel d. karpman <gdkar@alum.mit.edu>
2016-09-26 06:38:11 -0400

    control pot simplification.

--- src/control/controlpotmeter.cpp
+++ src/control/controlpotmeter.cpp
@@ -29,8 +29,8 @@ ControlPotmeter::ControlPotmeter(ConfigKey key,QObject *p,  double dMinValue, do
                                  bool bIgnoreNops,
                                  bool bTrack,
                                  bool bPersist)
-        : ControlObject(key, p, bIgnoreNops, bTrack, bPersist),
-          m_controls(key)
+        : ControlObject(key, p, bIgnoreNops, bTrack, bPersist)
+//          m_controls(key)
 {
     setRange(dMinValue, dMaxValue, bClampValue);
     auto default_value = dMinValue + 0.5 * (dMaxValue - dMinValue);
@@ -41,105 +41,59 @@ ControlPotmeter::ControlPotmeter(ConfigKey key,QObject *p,  double dMinValue, do
     //qDebug() << "" << this << ", min " << m_dMinValue << ", max " << m_dMaxValue << ", range " << m_dValueRange << ", val " << m_dValue;
 }
 ControlPotmeter::~ControlPotmeter() = default;
-
 void ControlPotmeter::setStepCount(int count)
 {
-    m_controls.setStepCount(count);
+    if(m_stepCount != count) {
+        m_stepCount = count;//m_controls.setStepCount(count);
+        emit stepCountChanged(count);
+    }
 }
-
 void ControlPotmeter::setSmallStepCount(int count)
 {
-    m_controls.setSmallStepCount(count);
+    if(m_smallStepCount != count) {
+        m_smallStepCount = count;//m_controls.setSmallStepCount(count);
+        emit smallStepCountChanged(count);
+    }
 }
-
 void ControlPotmeter::setRange(double dMinValue, double dMaxValue,bool allowOutOfBounds)
 {
+    auto unconstrain_changed = m_bAllowOutOfBounds != allowOutOfBounds;
     m_bAllowOutOfBounds = allowOutOfBounds;
+    auto min_changed = m_min != dMinValue;
+    m_min = dMinValue;
+    auto max_changed = m_max != dMaxValue;
+    m_max = dMaxValue;
     if (m_pControl) {
         m_pControl->setBehavior(new ControlPotmeterBehavior(dMinValue, dMaxValue, allowOutOfBounds));
     }
+    if(unconstrain_changed)
+        unconstrainChanged(allowOutOfBounds);
+    if(min_changed)
+        minValueChanged(dMinValue);
+    if(max_changed)
+        maxValueChanged(dMaxValue);
 }
-
-PotmeterControls::PotmeterControls(const ConfigKey& key)
-        : m_pControl(new ControlProxy(key, this)),
-          m_stepCount(10),
-          m_smallStepCount(100) {
-    // These controls are deleted when the ControlPotmeter is since
-    // PotmeterControls is a member variable of the associated ControlPotmeter
-    // and the push-button controls are parented to the PotmeterControls.
-
-    auto controlUp = new ControlPushButton(
-        ConfigKey(key.group, QString(key.item) + "_up"),this);
-    connect(controlUp, &ControlObject::valueChanged,this, &PotmeterControls::incValue);
-
-    auto controlDown = new ControlPushButton(
-        ConfigKey(key.group, QString(key.item) + "_down"),this);
-    connect(controlDown, &ControlObject::valueChanged,this, &PotmeterControls::decValue);
-
-    auto controlUpSmall = new ControlPushButton(
-        ConfigKey(key.group, QString(key.item) + "_up_small"),this);
-    connect(controlUpSmall, &ControlObject::valueChanged,this, &PotmeterControls::incSmallValue);
-
-    auto controlDownSmall = new ControlPushButton(
-        ConfigKey(key.group, QString(key.item) + "_down_small"),this);
-    connect(controlDownSmall, &ControlObject::valueChanged,this, &PotmeterControls::decSmallValue);
-
-    auto controlDefault = new ControlPushButton(
-        ConfigKey(key.group, QString(key.item) + "_set_default"),this);
-    connect(controlDefault, &ControlObject::valueChanged,
-            this, &PotmeterControls::setToDefault);
-
-    auto controlToggle = new ControlPushButton(
-        ConfigKey(key.group, QString(key.item) + "_toggle"),this);
-    connect(controlToggle, &ControlObject::valueChanged,
-            this, &PotmeterControls::toggleValue);
-
-}
-
-PotmeterControls::~PotmeterControls() {
-}
-
-void PotmeterControls::incValue(double v)
+double ControlPotmeter::minValue() const { return m_min;}
+double ControlPotmeter::maxValue() const { return m_max;}
+bool  ControlPotmeter::unconstrain() const { return m_bAllowOutOfBounds;}
+int ControlPotmeter::stepCount() const { return m_stepCount;}
+int ControlPotmeter::smallStepCount() const { return m_smallStepCount;}
+void ControlPotmeter::setMinValue(double _min)
 {
-    if (v > 0) {
-        m_pControl->fetch_add(1./m_stepCount);
+    if(_min != m_min) {
+        setRange(_min, maxValue(), unconstrain());
     }
 }
-void PotmeterControls::decValue(double v)
+void ControlPotmeter::setMaxValue(double _max)
 {
-    if (v > 0) {
-        m_pControl->fetch_add(-1./m_stepCount);
+    if(_max != m_max) {
+        setRange(minValue(), _max, unconstrain());
     }
 }
-void PotmeterControls::incSmallValue(double v)
+void ControlPotmeter::setUnconstrain(bool x)
 {
-    if (v > 0) {
-        m_pControl->fetch_add(1./m_smallStepCount);
+    if(m_bAllowOutOfBounds!= x) {
+        setRange(minValue(), maxValue(), x);
     }
 }
-void PotmeterControls::decSmallValue(double v)
-{
-    if (v > 0) {
-        m_pControl->fetch_add(-1./m_smallStepCount);
-    }
-}
-void PotmeterControls::setToDefault(double v)
-{
-    if (v > 0) {
-        m_pControl->reset();
-    }
-}
-void PotmeterControls::toggleValue(double v)
-{
-    if (v > 0) {
-        m_pControl->fetch_toggle();
-    }
-}
-void PotmeterControls::setStepCount(int count)
-{
-    m_stepCount = count;
-}
-void PotmeterControls::setSmallStepCount(int count)
-{
-    m_smallStepCount = count;
-}
+
--- src/control/controlpotmeter.h
+++ src/control/controlpotmeter.h
@@ -28,7 +28,7 @@
 class ControlPushButton;
 class ControlProxy;
 
-class PotmeterControls : public QObject {
+/*class PotmeterControls : public QObject {
     Q_OBJECT
   public:
     PotmeterControls(const ConfigKey& key);
@@ -54,10 +54,15 @@ class PotmeterControls : public QObject {
     ControlProxy* m_pControl;
     int m_stepCount;
     double m_smallStepCount;
-};
+};*/
 
 class ControlPotmeter : public ControlObject {
     Q_OBJECT
+    Q_PROPERTY(double min READ minValue WRITE setMinValue NOTIFY minValueChanged);
+    Q_PROPERTY(double max READ maxValue WRITE setMaxValue NOTIFY maxValueChanged);
+    Q_PROPERTY(bool unconstrain READ unconstrain WRITE setUnconstrain NOTIFY unconstrainChanged);
+    Q_PROPERTY(int stepCount READ stepCount WRITE setStepCount NOTIFY stepCountChanged);
+    Q_PROPERTY(int smallStepCount READ smallStepCount WRITE setSmallStepCount NOTIFY smallStepCountChanged);
   public:
     ControlPotmeter(ConfigKey key, double dMinValue = 0.0, double dMaxValue = 1.0,
                     bool allowOutOfBounds = false,
@@ -73,14 +78,32 @@ class ControlPotmeter : public ControlObject {
     virtual ~ControlPotmeter();
     // Sets the step count of the associated PushButtons.
     void setStepCount(int count);
+    int stepCount() const;
     // Sets the small step count of the associated PushButtons.
     void setSmallStepCount(int count);
+    int smallStepCount() const;
     // Sets the minimum and maximum allowed value. The control value is reset
     // when calling this method
     void setRange(double dMinValue, double dMaxValue, bool allowOutOfBounds);
+    double minValue() const;
+    double maxValue() const;
+    bool   unconstrain() const;
+    void setMinValue(double);
+    void setMaxValue(double);
+    void setUnconstrain(bool);
+  signals:
+    void unconstrainChanged(bool);
+    void minValueChanged(double);
+    void maxValueChanged(double);
+    void stepCountChanged(int);
+    void smallStepCountChanged(int);
   protected:
     bool m_bAllowOutOfBounds;
-    PotmeterControls m_controls;
+//    PotmeterControls m_controls;
+    int m_stepCount;
+    double m_smallStepCount;
+    double m_min;
+    double m_max;
 };
 
 #endif
