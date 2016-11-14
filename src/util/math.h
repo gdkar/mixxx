1d1aeec71091f5795ec7329a5467679cf7edcdc8
gabriel d. karpman <gdkar@alum.mit.edu>
2016-11-13 20:49:08 -0500

    make build take sliightly less forever.

--- src/util/math.h
+++ src/util/math.h
@@ -10,14 +10,19 @@
 #endif
 #endif
 
-#include <math.h>
-#include <cmath> 
-// Note: Because of our fpclassify hack, we actualy need to inlude both, 
-// the c and the c++ version of the math header.  
-// From GCC 6.1.1 math.h depends on cmath, which failes to compile if included 
-// after our fpclassify hack 
-
+#include <cmath>
+#include <numeric>
 #include <algorithm>
+#include <utility>
+#include <limits>
+#include <math.h>
+// Note: Because of our fpclassify hack, we actualy need to inlude both,
+// the c and the c++ version of the math header.
+// From GCC 6.1.1 math.h depends on cmath, which failes to compile if included
+// after our fpclassify hack
+
+#include <functional>
+#include <type_traits>
 
 #include "util/assert.h"
 #include "util/fpclassify.h"
@@ -28,51 +33,63 @@ using std::fabs;
 
 #define math_max std::max
 #define math_min std::min
-#define math_max3(a, b, c) math_max(math_max((a), (b)), (c))
+constexpr decltype(auto) math_max3(auto && a, auto && b, auto && c)
+{
+    return std::max({a,b,c});
+}
+template<class T,class Compare>
+constexpr const T& clamp(const T& v, const T& lo, const T& hi, Compare comp)
+{
+    return comp(v,lo) ? lo : comp(hi,v) ? hi : v;
+}
+template<class T>
+constexpr const T& clamp(const T& v, const T& lo, const T& hi)
+{
+    return clamp(v, lo, hi, std::less<>());
+}
 
 // Restrict value to the range [min, max]. Undefined behavior if min > max.
 template <typename T>
-inline T math_clamp(T value, T min, T max) {
+constexpr T math_clamp(T value, T _min, T _max) {
     // DEBUG_ASSERT compiles out in release builds so it does not affect
     // vectorization or pipelining of clamping in tight loops.
-    DEBUG_ASSERT(min <= max);
-    return math_max(min, math_min(max, value));
+    return clamp(value, _min,_max);
 }
 
 // NOTE(rryan): It is an error to call even() on a floating point number. Do not
 // hack this to support floating point values! The programmer should be required
 // to manually convert so they are aware of the conversion.
 template <typename T>
-inline bool even(T value) {
-    return value % 2 == 0;
+constexpr typename std::enable_if<std::is_integral<T>::value,bool>::type even(T value)
+{
+    return !(value & T{1});
 }
 
 #ifdef _MSC_VER
 // Ask VC++ to emit an intrinsic for fabs instead of calling std::fabs.
 #pragma intrinsic(fabs)
 #endif
-
-inline int roundUpToPowerOf2(int v) {
-    int power = 1;
-    while (power < v && power > 0) {
-        power *= 2;
-    }
-    // There is not a power of 2 higher than v representable by our
-    // architecture's integer size.
-    if (power < 0) {
-        return -1;
-    }
-    return power;
+namespace detail {
+template<class T>
+constexpr typename std::enable_if<std::is_integral<T>::value,T>::type roundUpToPowerOf2(T v, int shift)
+{
+    using U = typename std::make_unsigned<T>::type;
+    return (shift < (std::numeric_limits<U>::digits>>1)) ? roundUpToPowerOf2(U(v)|(U(v)>>shift),shift<<1) : (U(v)|(U(v)>>shift));
+}
+}
+template<class T>
+constexpr typename std::enable_if<std::is_integral<T>::value,T>::type roundUpToPowerOf2(T v)
+{
+    using U = typename std::make_unsigned<T>::type;
+    return T(U(1) + detail::roundUpToPowerOf2(U(v) - U(1),1));
 }
 
 template <typename T>
-inline const T ratio2db(const T a) {
-    return log10(a) * 20;
+constexpr const T ratio2db(const T a) {
+    return std::log10(a) * 20;
 }
-
 template <typename T>
-inline const T db2ratio(const T a) {
-    return pow(10, a / 20);
+constexpr const T db2ratio(const T a) {
+    return std::pow(10, a / 20);
 }
-
 #endif /* MATH_H */
--- src/util/types.h
+++ src/util/types.h
@@ -3,6 +3,13 @@
 
 #include <cstddef>
 #include <climits>
+#include <limits>
+#include <cstdint>
+#include <cstddef>
+#include <cmath>
+#include <numeric>
+#include <algorithm>
+#include <utility>
 
 #include "util/math.h"
 
@@ -11,24 +18,23 @@
 // This should be used for all CSAMLE operations since it is fast and
 // allows compiler auto vectorizing. For Qt container operations use
 // just int as before.
-typedef std::ptrdiff_t SINT;
+using SINT = std::ptrdiff_t;
 
 // 16-bit integer sample data within the asymmetric
 // range [SHRT_MIN, SHRT_MAX].
-typedef short int SAMPLE;
-constexpr SAMPLE SAMPLE_ZERO = 0;
-constexpr SAMPLE SAMPLE_MIN = SHRT_MIN;
-constexpr SAMPLE SAMPLE_MAX = SHRT_MAX;
+using SAMPLE = std::int16_t;
+constexpr auto SAMPLE_ZERO  = SAMPLE(0);
+constexpr auto SAMPLE_MIN = std::numeric_limits<SAMPLE>::min();
+constexpr auto SAMPLE_MAX = std::numeric_limits<SAMPLE>::max();
 
 // Limits the range of a SAMPLE value to [SAMPLE_MIN, SAMPLE_MAX].
-inline
-SAMPLE SAMPLE_clamp(SAMPLE in) {
+constexpr SAMPLE SAMPLE_clamp(SAMPLE in) {
     return math_clamp(in, SAMPLE_MIN, SAMPLE_MAX);
 }
 
 // Limits the range of a SAMPLE value to [-SAMPLE_MAX, SAMPLE_MAX].
-inline
-SAMPLE SAMPLE_clampSymmetric(SAMPLE in) {
+
+constexpr SAMPLE SAMPLE_clampSymmetric(SAMPLE in) {
     return math_clamp(in, static_cast<SAMPLE>(-SAMPLE_MAX), SAMPLE_MAX);
 }
 
@@ -37,29 +43,27 @@ SAMPLE SAMPLE_clampSymmetric(SAMPLE in) {
 // amplitude of 1.0. No min/max constants here to
 // emphasize the symmetric value range of CSAMPLE
 // data!
-typedef float CSAMPLE;
-constexpr CSAMPLE CSAMPLE_ZERO = 0.0f;
-constexpr CSAMPLE CSAMPLE_ONE = 1.0f;
-constexpr CSAMPLE CSAMPLE_PEAK = CSAMPLE_ONE;
+using CSAMPLE = float;
+constexpr auto  CSAMPLE_ZERO = CSAMPLE(0.0f);
+constexpr auto CSAMPLE_ONE = CSAMPLE(1.0f);
+constexpr auto CSAMPLE_PEAK = CSAMPLE_ONE;
 
 // Limits the range of a CSAMPLE value to [-CSAMPLE_PEAK, CSAMPLE_PEAK].
-inline
-CSAMPLE CSAMPLE_clamp(CSAMPLE in) {
+constexpr CSAMPLE CSAMPLE_clamp(CSAMPLE in) {
     return math_clamp(in, -CSAMPLE_PEAK, CSAMPLE_PEAK);
 }
 
 // Gain values for weighted calculations of CSAMPLE
 // data in the range [0.0, 1.0]. Same data type as
 // CSAMPLE to avoid type conversions in calculations.
-typedef CSAMPLE CSAMPLE_GAIN;
-constexpr float CSAMPLE_GAIN_ZERO = CSAMPLE_ZERO;
-constexpr float CSAMPLE_GAIN_ONE = CSAMPLE_ONE;
-constexpr float CSAMPLE_GAIN_MIN = CSAMPLE_GAIN_ZERO;
-constexpr float CSAMPLE_GAIN_MAX = CSAMPLE_GAIN_ONE;
+using CSAMPLE_GAIN = CSAMPLE;
+constexpr auto CSAMPLE_GAIN_ZERO = CSAMPLE_ZERO;
+constexpr auto CSAMPLE_GAIN_ONE = CSAMPLE_ONE;
+constexpr auto CSAMPLE_GAIN_MIN = CSAMPLE_GAIN_ZERO;
+constexpr auto CSAMPLE_GAIN_MAX = CSAMPLE_GAIN_ONE;
 
 // Limits the range of a CSAMPLE_GAIN value to [CSAMPLE_GAIN_MIN, CSAMPLE_GAIN_MAX].
-inline
-CSAMPLE_GAIN CSAMPLE_GAIN_clamp(CSAMPLE_GAIN in) {
+constexpr CSAMPLE_GAIN CSAMPLE_GAIN_clamp(CSAMPLE_GAIN in) {
     return math_clamp(in, CSAMPLE_GAIN_MIN, CSAMPLE_GAIN_MAX);
 }
 
