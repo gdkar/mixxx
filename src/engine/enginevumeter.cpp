5c5b46d3b8b1de0fb7a97f13f00d77d72e52b07f
gabriel d. karpman <gdkar@alum.mit.edu>
2016-11-13 20:49:08 -0500

    make build take sliightly less forever.

--- src/engine/enginevumeter.cpp
+++ src/engine/enginevumeter.cpp
@@ -60,8 +60,7 @@ void EngineVuMeter::process(CSAMPLE* pIn, const int iBufferSize) {
 
     int sampleRate = (int)m_pSampleRate->get();
 
-    SampleUtil::CLIP_STATUS clipped = SampleUtil::sumAbsPerChannel(&fVolSumL,
-            &fVolSumR, pIn, iBufferSize);
+    auto clipped = SampleUtil::sumAbsPerChannel(&fVolSumL,&fVolSumR, pIn, iBufferSize);
     m_fRMSvolumeSumL += fVolSumL;
     m_fRMSvolumeSumR += fVolSumR;
 
@@ -70,11 +69,9 @@ void EngineVuMeter::process(CSAMPLE* pIn, const int iBufferSize) {
     // Are we ready to update the VU meter?:
     if (m_iSamplesCalculated > (sampleRate / VU_UPDATE_RATE)) {
         doSmooth(m_fRMSvolumeL,
-                log10(SHRT_MAX * m_fRMSvolumeSumL
-                                / (m_iSamplesCalculated * 1000) + 1));
+                log10(SHRT_MAX * m_fRMSvolumeSumL/ (m_iSamplesCalculated * 1000) + 1));
         doSmooth(m_fRMSvolumeR,
-                log10(SHRT_MAX * m_fRMSvolumeSumR
-                                / (m_iSamplesCalculated * 1000) + 1));
+                log10(SHRT_MAX * m_fRMSvolumeSumR/ (m_iSamplesCalculated * 1000) + 1));
 
         const double epsilon = .0001;
 
--- src/util/math.h
+++ src/util/math.h
@@ -10,14 +10,18 @@
 #endif
 #endif
 
+#include <cmath>
 #include <math.h>
-#include <cmath> 
-// Note: Because of our fpclassify hack, we actualy need to inlude both, 
-// the c and the c++ version of the math header.  
-// From GCC 6.1.1 math.h depends on cmath, which failes to compile if included 
-// after our fpclassify hack 
+// Note: Because of our fpclassify hack, we actualy need to inlude both,
+// the c and the c++ version of the math header.
+// From GCC 6.1.1 math.h depends on cmath, which failes to compile if included
+// after our fpclassify hack
 
 #include <algorithm>
+#include <utility>
+#include <numeric>
+#include <functional>
+#include <type_traits>
 
 #include "util/assert.h"
 #include "util/fpclassify.h"
@@ -32,54 +36,52 @@ using std::fabs;
 
 // Restrict value to the range [min, max]. Undefined behavior if min > max.
 template <typename T>
-inline T math_clamp(T value, T min, T max) {
+constexpr T math_clamp(T value, T min, T max) {
     // DEBUG_ASSERT compiles out in release builds so it does not affect
     // vectorization or pipelining of clamping in tight loops.
-    DEBUG_ASSERT(min <= max);
-    return math_max(min, math_min(max, value));
+    return math_max<T>(min, math_min<T>(max, value));
 }
 
 // NOTE(rryan): It is an error to call even() on a floating point number. Do not
 // hack this to support floating point values! The programmer should be required
 // to manually convert so they are aware of the conversion.
 template <typename T>
-inline bool even(T value) {
-    return value % 2 == 0;
+constexpr typename std::enable_if<std::is_integral<T>::value,bool>::type even(T value)
+{
+    return !(value & T{1});
 }
 
 #ifdef _MSC_VER
 // Ask VC++ to emit an intrinsic for fabs instead of calling std::fabs.
 #pragma intrinsic(fabs)
 #endif
-
-inline int roundUpToPowerOf2(int v) {
-    int power = 1;
-    while (power < v && power > 0) {
-        power *= 2;
-    }
-    // There is not a power of 2 higher than v representable by our
-    // architecture's integer size.
-    if (power < 0) {
-        return -1;
-    }
-    return power;
+namespace detail {
+template<class T>
+constexpr typename std::enable_if<std::is_integral<T>::value,T>::type roundUpToPowerOf2(T v, int shift)
+{
+    using U = typename std::make_unsigned<T>::type;
+    return (shift < (std::numeric_limits<U>::digits>>1)) ? roundUpToPowerOf2(U(v)|(U(v)>>shift),shift<<1) : (U(v)|(U(v)>>shift));
+}
+}
+template<class T>
+constexpr typename std::enable_if<std::is_integral<T>::value,T>::type roundUpToPowerOf2(T v)
+{
+    using U = typename std::make_unsigned<T>::type;
+    return T(U(1) + detail::roundUpToPowerOf2(U(v) - U(1),1));
 }
 
 // MSVS 2013 (_MSC_VER 1800) introduced C99 support.
 #if defined(__WINDOWS__) &&  _MSC_VER < 1800
-inline int round(double x) {
-    return x < 0.0 ? ceil(x - 0.5) : floor(x + 0.5);
+constexpr int round(double x) {
+    return x < 0.0 ? std::ceil(x - 0.5) : std::floor(x + 0.5);
 }
 #endif
-
 template <typename T>
-inline const T ratio2db(const T a) {
-    return log10(a) * 20;
+constexpr const T ratio2db(const T a) {
+    return std::log10(a) * 20;
 }
-
 template <typename T>
-inline const T db2ratio(const T a) {
-    return pow(10, a / 20);
+constexpr const T db2ratio(const T a) {
+    return std::pow(10, a / 20);
 }
-
 #endif /* MATH_H */
