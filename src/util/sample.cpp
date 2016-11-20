2d091db8397b347bf5a65633585b877aee6f4366
gabriel d. karpman <gdkar@alum.mit.edu>
2016-11-20 04:17:11 -0500

    util/sample.cpp tweaking

--- src/util/sample.cpp
+++ src/util/sample.cpp
@@ -43,30 +43,27 @@ void SampleUtil::free(CSAMPLE* pBuffer)
     delete[] pBuffer;
 }
 
-// static
 void SampleUtil::copyWithGain(CSAMPLE *pDest, const CSAMPLE *pSrc, CSAMPLE_GAIN gain, SINT num)
 {
-    std::transform(&pSrc[0],&pSrc[num],&pDest[0],[=](CSAMPLE x){return x * gain;});
+    std::transform(&pSrc[0],&pSrc[num],&pDest[0],[gain](auto x){return x * gain;});
 }
-// static
 void SampleUtil::copyWithRampingGain(CSAMPLE *pDest, const CSAMPLE *pSrc, CSAMPLE_GAIN gain_pre, CSAMPLE_GAIN gain_post, SINT num)
 {
     if(gain_post == gain_pre)
         copyWithGain(pDest,pSrc,gain_pre,num);
     else {
-        auto step = (gain_post - gain_pre) * (2 / num);
-        num /= 2;
-        for(auto i = 0; i < num; i ++) {
+        num >>= 1;
+        auto step = (gain_post - gain_pre) / num;
+        for(auto i = 0l; i < num; ++i) {
             pDest[i * 2 + 0] = pSrc[i * 2 + 0] * gain_pre;
             pDest[i * 2 + 1] = pSrc[i * 2 + 1] * gain_pre;
             gain_pre += step;
         }
     }
 }
-// static
 void SampleUtil::addWithGain(CSAMPLE *pDest, const CSAMPLE *pSrc, CSAMPLE_GAIN gain, SINT num)
 {
-    for(auto i = 0; i < num; i ++ )
+    for(auto i = 0l; i < num; ++i)
         pDest[i] += pSrc[i] * gain;
 }
 void SampleUtil::addWithRampingGain(CSAMPLE *pDest, const CSAMPLE *pSrc, CSAMPLE_GAIN gain_pre, CSAMPLE_GAIN gain_post, SINT num)
@@ -74,21 +71,35 @@ void SampleUtil::addWithRampingGain(CSAMPLE *pDest, const CSAMPLE *pSrc, CSAMPLE
     if(gain_post == gain_pre) {
         addWithGain(pDest,pSrc,gain_pre,num);
     } else {
-        auto step = (gain_post - gain_pre) * (2 / num);
         num /= 2;
-        for(auto i = 0; i < num; i ++) {
+        auto step = (gain_post - gain_pre) / num;
+        for(auto i = 0l; i < num; ++i) {
             pDest[i * 2 + 0] += pSrc[i * 2 + 0] * gain_pre;
             pDest[i * 2 + 1] += pSrc[i * 2 + 1] * gain_pre;
             gain_pre += step;
         }
     }
 }
-void SampleUtil::applyGain(CSAMPLE *pBuffer, CSAMPLE_GAIN gain, SINT num)
+void SampleUtil::applyGain(CSAMPLE *pSrc, CSAMPLE_GAIN gain, SINT num)
 {
-    copyWithGain(pBuffer,pBuffer,gain,num);
+    std::transform(pSrc,pSrc + num, pSrc,[gain](auto x){return x * gain;});
 }
 void SampleUtil::applyRampingGain(CSAMPLE *pBuffer, CSAMPLE_GAIN gain_pre, CSAMPLE_GAIN gain_post, SINT num)
 {
+    if(gain_post == gain_pre) {
+        if(gain_pre) {
+            std::transform(pBuffer ,pBuffer+ num, pBuffer,[gain_pre](auto x){return x * gain_pre;});
+        }
+    } else {
+        num >>= 1;
+        auto step = (gain_post - gain_pre) / num;
+        for(auto i = 0l; i < num; ++i) {
+            pBuffer[i * 2 + 0] *= gain_pre;
+            pBuffer[i * 2 + 1] *= gain_pre;
+            gain_pre += step;
+        }
+    }
+
     copyWithRampingGain(pBuffer,pBuffer,gain_pre, gain_post,num);
 }
 // static
@@ -96,90 +107,87 @@ void SampleUtil::applyAlternatingGain(CSAMPLE* pBuffer, CSAMPLE gain1,
         CSAMPLE gain2, SINT iNumSamples)
 {
     // This handles gain1 == CSAMPLE_GAIN_ONE && gain2 == CSAMPLE_GAIN_ONE as well.
-    if (gain1 == gain2)
-        return applyGain(pBuffer, gain1, iNumSamples);
+    if (gain1 == gain2 && gain1 == CSAMPLE_GAIN_ONE)
+        return;
+//        return applyGain(pBuffer, gain1, iNumSamples);
     // note: LOOP VECTORIZED.
-    for (SINT i = 0; i < iNumSamples / 2; ++i) {
-        pBuffer[i * 2 + 0] *= gain1;
-        pBuffer[i * 2 + 1] *= gain2;
+    iNumSamples >>= 1;
+    for (SINT i = 0l; i < iNumSamples; ++i) {
+        pBuffer[i * 2 + 0l] *= gain1;
+        pBuffer[i * 2 + 1l] *= gain2;
     }
 }
-
 // static
 void SampleUtil::convertS16ToFloat32(CSAMPLE*  pDest, const SAMPLE*  pSrc,
-        SINT iNumSamples) {
+        SINT iNumSamples)
+{
     // SAMPLE_MIN = -32768 is a valid low sample, whereas SAMPLE_MAX = 32767
     // is the highest valid sample. Note that this means that although some
     // sample values convert to -1.0, none will convert to +1.0.
-    DEBUG_ASSERT(-SAMPLE_MIN >= SAMPLE_MAX);
-    constexpr CSAMPLE kConversionFactor = 1./-SAMPLE_MIN;
+    static_assert(-SAMPLE_MIN >= SAMPLE_MAX,"-SAMPLE_MIN must be >= SAMPLE_MAX");
+    constexpr CSAMPLE kConversionFactor = CSAMPLE{1}/-SAMPLE_MIN;
     // note: LOOP VECTORIZED.
     std::transform(pSrc,pSrc + iNumSamples, pDest,[=](auto x){return x * kConversionFactor;});
 }
 //static
 void SampleUtil::convertFloat32ToS16(SAMPLE* pDest, const CSAMPLE* pSrc,
         SINT iNumSamples) {
-    DEBUG_ASSERT(-SAMPLE_MIN >= SAMPLE_MAX);
+    static_assert(-SAMPLE_MIN >= SAMPLE_MAX,"-SAMPLE_MIN must be >= SAMPLE_MAX");
     constexpr CSAMPLE kConversionFactor = -SAMPLE_MIN;
-    std::transform(pSrc,pSrc + iNumSamples, pDest,[=](auto x){return x * kConversionFactor;});
+    std::transform(pSrc,pSrc + iNumSamples, pDest,[=](auto x){return SAMPLE(x * kConversionFactor);});
 }
-
 // static
 SampleUtil::CLIP_FLAGS SampleUtil::sumAbsPerChannel(CSAMPLE* pfAbsL, CSAMPLE* pfAbsR,
-        const CSAMPLE* pBuffer, SINT iNumSamples) {
-    CSAMPLE fAbsL = CSAMPLE_ZERO;
-    CSAMPLE fAbsR = CSAMPLE_ZERO;
-    CSAMPLE clippedL = 0;
-    CSAMPLE clippedR = 0;
-
+        const CSAMPLE* pBuffer, SINT iNumSamples)
+{
+    auto fAbsL = CSAMPLE_ZERO;
+    auto fAbsR = CSAMPLE_ZERO;
+    auto clippedL = CSAMPLE_ZERO;
+    auto clippedR = CSAMPLE_ZERO;
     // note: LOOP VECTORIZED.
-    for (SINT i = 0; i < iNumSamples / 2; ++i) {
-        CSAMPLE absl = fabs(pBuffer[i * 2]);
-        fAbsL += absl;
-        clippedL += absl > CSAMPLE_PEAK ? 1 : 0;
-        CSAMPLE absr = fabs(pBuffer[i * 2 + 1]);
-        fAbsR += absr;
+    iNumSamples >>= 1;
+    for (auto i = 0l; i < iNumSamples; ++i) {
+        auto absL = std::abs(pBuffer[i * 2 + 0l]);
+        auto absR = std::abs(pBuffer[i * 2 + 1l]);
+        fAbsL += absL;
+        fAbsR += absR;
+        clippedL += (absL > CSAMPLE_PEAK) ? CSAMPLE_ONE : CSAMPLE_ZERO;
+        clippedR += (absR > CSAMPLE_PEAK) ? CSAMPLE_ONE : CSAMPLE_ZERO;
         // Replacing the code with a bool clipped will prevent vetorizing
-        clippedR += absr > CSAMPLE_PEAK ? 1 : 0;
     }
 
     *pfAbsL = fAbsL;
     *pfAbsR = fAbsR;
     auto clipping = static_cast<SampleUtil::CLIP_FLAGS>(SampleUtil::CLIPPING_NONE);
-    if (clippedL > 0)
-        clipping |= SampleUtil::CLIPPING_LEFT;
-    if (clippedR > 0)
-        clipping |= SampleUtil::CLIPPING_RIGHT;
+    if (clippedL) clipping |= SampleUtil::CLIPPING_LEFT;
+    if (clippedR) clipping |= SampleUtil::CLIPPING_RIGHT;
     return clipping;
 }
-
 // static
 void SampleUtil::copyClampBuffer(CSAMPLE*  pDest, const  CSAMPLE* pSrc,
-        SINT iNumSamples) {
+        SINT iNumSamples)
+{
     // note: LOOP VECTORIZED.
     std::transform(pSrc,pSrc + iNumSamples,pDest,clampSample);
 }
-
 // static
 void SampleUtil::interleaveBuffer(CSAMPLE*  pDest, const CSAMPLE*  pSrc1,
         const CSAMPLE*  pSrc2, SINT iNumSamples) {
     // note: LOOP VECTORIZED.
-    for (SINT i = 0; i < iNumSamples; ++i) {
+    for (auto i = 0l; i < iNumSamples; ++i) {
         pDest[2 * i + 0] = pSrc1[i];
         pDest[2 * i + 1] = pSrc2[i];
     }
 }
-
 // static
 void SampleUtil::deinterleaveBuffer(CSAMPLE* pDest1, CSAMPLE* pDest2,
         const CSAMPLE* pSrc, SINT iNumSamples) {
     // note: LOOP VECTORIZED.
-    for (SINT i = 0; i < iNumSamples; ++i) {
-        pDest1[i] = pSrc[i * 2];
-        pDest2[i] = pSrc[i * 2 + 1];
+    for (auto i = 0l; i < iNumSamples; ++i) {
+        pDest1[i] = pSrc[i * 2 + 0l];
+        pDest2[i] = pSrc[i * 2 + 1l];
     }
 }
-
 // static
 void SampleUtil::linearCrossfadeBuffers(CSAMPLE* pDest,
         const CSAMPLE* pSrcFadeOut, const CSAMPLE* pSrcFadeIn,
@@ -187,7 +195,8 @@ void SampleUtil::linearCrossfadeBuffers(CSAMPLE* pDest,
     const CSAMPLE_GAIN cross_inc = CSAMPLE_GAIN_ONE
             / CSAMPLE_GAIN(iNumSamples / 2);
     // note: LOOP VECTORIZED.
-    for (SINT i = 0; i < iNumSamples / 2; ++i) {
+    iNumSamples >>= 1;
+    for (auto i = 0l; i < iNumSamples ; ++i) {
         const CSAMPLE_GAIN cross_mix = cross_inc * i;
         pDest[i * 2] = pSrcFadeIn[i * 2] * cross_mix
                 + pSrcFadeOut[i * 2] * (CSAMPLE_GAIN_ONE - cross_mix);
@@ -196,23 +205,23 @@ void SampleUtil::linearCrossfadeBuffers(CSAMPLE* pDest,
 
     }
 }
-
 // static
 void SampleUtil::mixStereoToMono(CSAMPLE* pDest, const CSAMPLE* pSrc,
-        SINT iNumSamples) {
-    const CSAMPLE_GAIN mixScale = CSAMPLE_GAIN_ONE
-            / (CSAMPLE_GAIN_ONE + CSAMPLE_GAIN_ONE);
+        SINT iNumSamples)
+{
+    constexpr auto mixScale = CSAMPLE_GAIN_ONE / (CSAMPLE_GAIN_ONE + CSAMPLE_GAIN_ONE);
     // note: LOOP VECTORIZED
-    for (SINT i = 0; i < iNumSamples / 2; ++i) {
-        pDest[i * 2] = (pSrc[i * 2] + pSrc[i * 2 + 1]) * mixScale;
-        pDest[i * 2 + 1] = pDest[i * 2];
+    for (auto i = 0l; i < iNumSamples / 2; ++i) {
+        auto val = (pSrc[i * 2 + 0l] + pSrc[i * 2 + 1l]) * mixScale;
+        pDest[i * 2 + 0l] = val;
+        pDest[i * 2 + 1l] = val;
     }
 }
-
 // static
-void SampleUtil::doubleMonoToDualMono(CSAMPLE* pBuffer, SINT numFrames) {
+void SampleUtil::doubleMonoToDualMono(CSAMPLE* pBuffer, SINT numFrames)
+{
     // backward loop
-    SINT i = numFrames;
+    auto i = numFrames;
     // Unvectorizable Loop
     while (0 < i--) {
         auto s = pBuffer[i];
@@ -220,10 +229,10 @@ void SampleUtil::doubleMonoToDualMono(CSAMPLE* pBuffer, SINT numFrames) {
         pBuffer[i * 2 + 1] = s;
     }
 }
-
 // static
 void SampleUtil::copyMonoToDualMono(CSAMPLE*  pDest, const CSAMPLE*  pSrc,
-        SINT numFrames) {
+        SINT numFrames)
+{
     // forward loop
     // note: LOOP VECTORIZED
     for (SINT i = 0; i < numFrames; ++i) {
@@ -252,14 +261,12 @@ void SampleUtil::copyMultiToStereo(CSAMPLE*  pDest, const CSAMPLE*  pSrc,
         pDest[i * 2 + 1] = pSrc[i * numChannels + 1];
     }
 }
-
-
 // static
 void SampleUtil::reverse(CSAMPLE* pBuffer, SINT iNumSamples) {
     for (SINT j = 0; j < iNumSamples / 4; ++j) {
         const SINT endpos = (iNumSamples - 1) - j * 2 ;
-        CSAMPLE temp1 = pBuffer[j * 2];
-        CSAMPLE temp2 = pBuffer[j * 2 + 1];
+        auto temp1 = pBuffer[j * 2];
+        auto temp2 = pBuffer[j * 2 + 1];
         pBuffer[j * 2] = pBuffer[endpos - 1];
         pBuffer[j * 2 + 1] = pBuffer[endpos];
         pBuffer[endpos - 1] = temp1;
@@ -275,4 +282,3 @@ void SampleUtil::copyReverse(CSAMPLE*  pDest, const CSAMPLE*  pSrc,
         pDest[j * 2 + 1] = pSrc[endpos];
     }
 }
-
