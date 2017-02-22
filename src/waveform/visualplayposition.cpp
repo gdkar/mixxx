f0c01bfd3e96aaf52eb5504e6f0134c38edc2937
gabriel d. karpman <gdkar@alum.mit.edu>
2017-02-21 22:04:01 -0500

    unbreak this. not sure why it was broken; but do not actually care

--- src/waveform/visualplayposition.cpp
+++ src/waveform/visualplayposition.cpp
@@ -14,23 +14,24 @@ double VisualPlayPosition::m_dCallbackEntryToDacSecs = 0;
 
 VisualPlayPosition::VisualPlayPosition(const QString& key)
         : m_valid(false),
-          m_key(key) {
-    m_audioBufferSize = new ControlProxy(
-            "[Master]", "audio_buffer_size", this);
-    m_audioBufferSize->connectValueChanged(
-            SLOT(slotAudioBufferSizeChanged(double)));
+          m_key(key)
+{
+    m_audioBufferSize = new ControlProxy("[Master]", "audio_buffer_size", this);
+    m_audioBufferSize->connectValueChanged(SLOT(slotAudioBufferSizeChanged(double)));
     m_dAudioBufferSize = m_audioBufferSize->get();
 }
 
-VisualPlayPosition::~VisualPlayPosition() {
+VisualPlayPosition::~VisualPlayPosition()
+{
     m_listVisualPlayPosition.remove(m_key);
 }
 
 void VisualPlayPosition::set(double playPos, double rate,
-                             double positionStep, double pSlipPosition) {
+                             double positionStep, double pSlipPosition)
+{
     VisualPlayPositionData data;
     data.m_referenceTime = m_timeInfoTime;
-    data.m_callbackEntrytoDac = m_dCallbackEntryToDacSecs * 1000000; // s to µs
+    data.m_callbackEntrytoDac = m_dCallbackEntryToDacSecs * 1e6; // s to µs
     data.m_enginePlayPos = playPos;
     data.m_rate = rate;
     data.m_positionStep = positionStep;
@@ -41,19 +42,20 @@ void VisualPlayPosition::set(double playPos, double rate,
     m_valid = true;
 }
 
-double VisualPlayPosition::getAtNextVSync(VSyncThread* vsyncThread) {
+double VisualPlayPosition::getAtNextVSync(VSyncThread* vsyncThread)
+{
     //static double testPos = 0;
     //testPos += 0.000017759; //0.000016608; //  1.46257e-05;
     //return testPos;
 
     if (m_valid) {
-        VisualPlayPositionData data = m_data.getValue();
-        int usRefToVSync = vsyncThread->usFromTimerToNextSync(data.m_referenceTime);
-        int offset = usRefToVSync - data.m_callbackEntrytoDac;
-        double playPos = data.m_enginePlayPos;  // load playPos for the first sample in Buffer
+        auto data = m_data.getValue();
+//        auto usRefToVSync = vsyncThread->usFromTimerToNextSync(data.m_referenceTime);
+//        auto offset = usRefToVSync - data.m_callbackEntrytoDac;
+        auto playPos = data.m_enginePlayPos;  // load playPos for the first sample in Buffer
         // add the offset for the position of the sample that will be transfered to the DAC
         // When the next display frame is displayed
-        playPos += data.m_positionStep * offset * data.m_rate / m_dAudioBufferSize / 1000;
+//        playPos += data.m_positionStep * offset * data.m_rate / m_dAudioBufferSize / 1000;
         //qDebug() << "delta Pos" << playPos - m_playPosOld << offset;
         //m_playPosOld = playPos;
         return playPos;
@@ -78,21 +80,24 @@ void VisualPlayPosition::getPlaySlipAt(int usFromNow, double* playPosition, doub
     }
 }
 
-double VisualPlayPosition::getEnginePlayPos() {
+double VisualPlayPosition::getEnginePlayPos()
+{
     if (m_valid) {
-        VisualPlayPositionData data = m_data.getValue();
+        auto data = m_data.getValue();
         return data.m_enginePlayPos;
     } else {
         return -1;
     }
 }
 
-void VisualPlayPosition::slotAudioBufferSizeChanged(double size) {
+void VisualPlayPosition::slotAudioBufferSizeChanged(double size)
+{
     m_dAudioBufferSize = size;
 }
 
 //static
-QSharedPointer<VisualPlayPosition> VisualPlayPosition::getVisualPlayPosition(QString group) {
+QSharedPointer<VisualPlayPosition> VisualPlayPosition::getVisualPlayPosition(QString group)
+{
     QSharedPointer<VisualPlayPosition> vpp = m_listVisualPlayPosition.value(group);
     if (vpp.isNull()) {
         vpp = QSharedPointer<VisualPlayPosition>(new VisualPlayPosition(group));
@@ -102,7 +107,8 @@ QSharedPointer<VisualPlayPosition> VisualPlayPosition::getVisualPlayPosition(QSt
 }
 
 //static
-void VisualPlayPosition::setCallbackEntryToDacSecs(double secs, const PerformanceTimer& time) {
+void VisualPlayPosition::setCallbackEntryToDacSecs(double secs, const PerformanceTimer& time)
+{
     // the time is valid only just NOW, so measure the time from NOW for
     // later correction
     m_timeInfoTime = time;
